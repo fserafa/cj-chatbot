9240f8d0251d521df55cd2233df7c03a5e6c6d8d
Felipe <felipe.seraf@gmail.comgit config --global user.name Felipegit coomit -m Firstgit config --global user.email felipe.seraf@gmail.com>
2020-03-11 10:51:37 -0300

    Updates

---
+++ precache-manifest.9802604d2c0887c64016283d2241da66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eedc92cb93a4d7aba26f5c8ff71d27d4",
+    "url": "/cj-chatbot/index.html"
+  },
+  {
+    "revision": "d3d51c0cecb52b1ec43d",
+    "url": "/cj-chatbot/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c25793b6655a7d1f6702",
+    "url": "/cj-chatbot/static/js/2.8a5c1e07.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/cj-chatbot/static/js/2.8a5c1e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d51c0cecb52b1ec43d",
+    "url": "/cj-chatbot/static/js/main.54dd8ff3.chunk.js"
+  },
+  {
+    "revision": "b58dc82386f86021e997",
+    "url": "/cj-chatbot/static/js/runtime-main.f0279e19.js"
+  }
+]);
